d1f2ae3d9ee27ecb68ffb7e612f841d34ca8e2b1
rdvelazquez <ryan.velazquez0001@temple.edu>
2018-09-11 07:04:09 -0400

    sort papers; add some more recent papers

--- src/data/papers_info.js
+++ src/data/papers_info.js
@@ -1,24 +1,59 @@
 export default {
-  "HyPhy": {
-    "title": "HyPhy: hypothesis testing using phylogenies",
-    "authors": "SLK Pond, SV Muse",
-    "publication": "Statistical methods in molecular evolution",
-    "date": "2005",
-    "link": "https://link.springer.com/chapter/10.1007%2F0-387-27733-1_6"
+  "Phyotree.js": {
+    "title": "phylotree. js - a JavaScript library for application development and interactive data visualization in phylogenetics",
+    "authors": "Stephen D Shank, Steven Weaver, Sergei L Kosakovsky Pond",
+    "publication": "BMC bioinformatics",
+    "date": "2018",
+    "link": "https://bmcbioinformatics.biomedcentral.com/articles/10.1186/s12859-018-2283-2?utm_campaign=Revue%20newsletter&utm_medium=Newsletter&utm_source=Programming%20Innovations"
+  },
+  "HIV clusters in NYC": {
+    "title": "Growth of HIV-1 Molecular Transmission Clusters in New York City",
+    "authors": "Joel O Wertheim, Ben Murrell, Sanjay R Mehta, Lisa A Forgione, Pond SL Kosakovsky, Davey M Smith, Lucia V Torian",
+    "publication": "The Journal of infectious diseases",
+    "date": "2018",
+    "link": "https://academic.oup.com/jid/advance-article-abstract/doi/10.1093/infdis/jiy431/5053881?redirectedFrom=PDF"
   },
   "HIV-TRACE": {
     "title": "Not so different after all: a comparison of methods for detecting amino acid sites under selection",
     "authors": "SL Kosakovsky Pond, SDW Frost",
     "publication": "Molecular biology and evolution",
-    "date": "2005",
+    "date": "2018",
     "link": "https://academic.oup.com/mbe/article-abstract/22/5/1208/1066893"
   },
-  "GARD": {
-    "title": "Automated phylogenetic detection of recombination using a genetic algorithm",
-    "authors": "SL Kosakovsky Pond, D Posada, MB Gravenor, CH Woelk, SDW Frost",
+  "Neutral Note": {
+    "title": "Neutral theory and rapidly evolving viral pathogens",
+    "authors": "Simon DW Frost, Brittany Rife Magalis, Sergei L Kosakovsky Pond",
     "publication": "Molecular biology and evolution",
-    "date": "2006",
-    "link": "https://academic.oup.com/mbe/article-abstract/23/10/1891/1096946"
+    "date": "2018",
+    "link": "https://academic.oup.com/mbe/article/35/6/1348/4983910"
+  },
+  "LEISR": {
+    "title": "Relative evolutionary rate inference in HyPhy with LEISR",
+    "authors": "Stephanie J Spielman, Sergei L Kosakovsky Pond",
+    "publication": "PeerJ",
+    "date": "2018",
+    "link": "https://peerj.com/articles/4339/?utm_source=TrendMD&utm_campaign=PeerJ_TrendMD_1&utm_medium=TrendMD"
+  },
+  "Tissue Reservoir": {
+    "title": "Persistent HIV-1 replication maintains the tissue reservoir during therapy",
+    "authors": "Ramon Lorenzo-Redondo, Helen R. Fryer, Trevor Bedford, Eun-Young Kim, John Archer, Sergei L. Kosakovsky Pond, Yoon-Seok Chung, Sudhir Penugonda, Jeffrey G. Chipman, Courtney V. Fletcher, Timothy W. Schacker, Michael H. Malim, Andrew Rambaut, Ashley T. Haase, Angela R. McLean, Steven M. Wolinsky",
+    "publication": "Nature",
+    "date": "2016",
+    "link": "https://www.nature.com/articles/nature16933"
+  },
+  "BUSTED": {
+    "title": "Gene-wide identification of episodic selection",
+    "authors": "Ben Murrell, Steven Weaver, Martin D Smith, Joel O Wertheim, Sasha Murrell, Anthony Aylward, Kemal Eren, Tristan Pollner, Darren P Martin, Davey M Smith, Konrad Scheffler, Sergei L Kosakovsky Pond",
+    "publication": "Molecular Biology and Evolution",
+    "date": "2015",
+    "link": "https://academic.oup.com/mbe/article/32/5/1365/1134918"
+  },
+  "FUBAR": {
+    "title": "FUBAR: A Fast, Unconstrained Bayesian AppRoximation for Inferring Selection",
+    "authors": "Ben Murrell, Sasha Moola, Amandla Mabona, Thomas Weighill, Daniel Sheward, Sergei L. Kosakovsky Pond, Konrad Scheffler",
+    "publication": "Molecular Biology and Evolution",
+    "date": "2013",
+    "link": "https://academic.oup.com/mbe/article/30/5/1196/998247"
   },
   "MEME": {
     "title": "Detecting individual sites subject to episodic diversifying selection",
@@ -34,18 +69,19 @@ export default {
     "date": "2007",
     "link": "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC1900087/"
   },
-  "FUBAR": {
-    "title": "FUBAR: A Fast, Unconstrained Bayesian AppRoximation for Inferring Selection",
-    "authors": "Ben Murrell, Sasha Moola, Amandla Mabona, Thomas Weighill, Daniel Sheward, Sergei L. Kosakovsky Pond, Konrad Scheffler",
-    "publication": "Molecular Biology and Evolution",
-    "date": "2013",
-    "link": "https://academic.oup.com/mbe/article/30/5/1196/998247"
+  "GARD": {
+    "title": "Automated phylogenetic detection of recombination using a genetic algorithm",
+    "authors": "SL Kosakovsky Pond, D Posada, MB Gravenor, CH Woelk, SDW Frost",
+    "publication": "Molecular biology and evolution",
+    "date": "2006",
+    "link": "https://academic.oup.com/mbe/article-abstract/23/10/1891/1096946"
   },
-  "Tissue Reservoir": {
-    "title": "Persistent HIV-1 replication maintains the tissue reservoir during therapy",
-    "authors": "Ramon Lorenzo-Redondo, Helen R. Fryer, Trevor Bedford, Eun-Young Kim, John Archer, Sergei L. Kosakovsky Pond, Yoon-Seok Chung, Sudhir Penugonda, Jeffrey G. Chipman, Courtney V. Fletcher, Timothy W. Schacker, Michael H. Malim, Andrew Rambaut, Ashley T. Haase, Angela R. McLean, Steven M. Wolinsky",
-    "publication": "Nature",
-    "date": "2016",
-    "link": "https://www.nature.com/articles/nature16933"
+    "HyPhy": {
+    "title": "HyPhy: hypothesis testing using phylogenies",
+    "authors": "SLK Pond, SV Muse",
+    "publication": "Statistical methods in molecular evolution",
+    "date": "2005",
+    "link": "https://link.springer.com/chapter/10.1007%2F0-387-27733-1_6"
   }
 }
+
