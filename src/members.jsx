b29633cca3bb138e4764076ac151a30317f60955
koreahnkim <koreahnkim@gmail.com>
2020-08-11 14:26:38 -0400

    retry update

--- src/members.jsx
+++ src/members.jsx
@@ -42,7 +42,10 @@ class Members extends Component {
           <a style={{ color: "black" }} href="http://www.azimuth1.com/about.html">
             <p style={{marginBottom: "0"}}>Ryan Velazquez - Lead Software Developer (currently Data Scientist at Azimuth1)</p>
           </a>
-          <p style={{marginBottom: "0"}}>Brittany Rife Magalis - Postdoctoral researcher (currently postdoctoral researcher at University of Florida)</p>
+          <p style={{marginBottom: "0"}}>Brittany Rife Magalis - Postdoctoral Researcher (currently Postdoctoral Researcher at University of Florida)</p>
+          <a style={{ color: "black" }} href="https://www.inovio.com/">
+            <p style={{marginBottom: "0"}}>Sadie Wisotsky - Postdoctoral Researcher (currently Bioinformatics Scientist at INOVIO)</p>
+          </a>
         </div>
 
       </div>
