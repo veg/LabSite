57d17c1f58f33ad12eb54c9ffc800baee06a9a7f
koreahnkim <koreahnkim@gmail.com>
2021-05-14 19:33:24 -0400

    The previous PR, but then with `git add`, and 7 files that were missing.

---
+++ src/data/papers_info_disease.js
@@ -0,0 +1,107 @@
+export default {
+  "Diversity of Functionally Distinct Clonal Sets": {
+    "title": "Diversity of Functionally Distinct Clonal Sets of Human Conventional Memory B Cells That Bind Staphylococcal Protein A",
+    "authors": "Emily E Radke, Zhi Li, David N Hernandez, Hanane El Bannoudi, Sergei L Kosakovsky Pond, Bo Shopsin, Peter Lopez, David Fenyö, Gregg J Silverman",
+    "publication": "Frontiers in Immunology",
+    "date": "2021",
+    "link": "https://www.frontiersin.org/articles/10.3389/fimmu.2021.662782/full"
+  },
+  "Effective HIV Molecular Surveillance Requires Identification of Incident Cases": {
+      "title": "Effective HIV Molecular Surveillance Requires Identification of Incident Cases of Infection",
+      "authors": "Susan J Little, Tom Chen, Rui Wang, Christy Anderson, Sergei Kosakovsky Pond, Masato Nakazawa, William C Mathews, Victor DeGruttola, Davey M Smith",
+      "publication": "Clinical Infectious Diseases: an Official Publication of the Infectious Diseases Society of America",
+      "date": "2021",
+      "link": "https://academic.oup.com/cid/advance-article-abstract/doi/10.1093/cid/ciab140/6137277"
+  },
+  "Molecular evolution of human norovirus GII. 2 clusters": {
+      "title": "Molecular evolution of human norovirus GII. 2 clusters",
+      "authors": "Xingguang Li, Haizhou Liu, Brittany Rife Magalis, Sergei L Kosakovsky Pond, Erik M Volz",
+      "publication": "Frontiers in microbiology",
+      "date": "2021",
+      "link": "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8019798/"
+  },
+  "Deep Sequencing Reveals Compartmentalized HIV-1 in the Semen": {
+      "title": "Deep Sequencing Reveals Compartmentalized HIV-1 in the Semen of Men with and without Sexually Transmitted Infection-Associated Urethritis",
+      "authors": "Olivia D Council, Shuntai Zhou, Chase D McCann, Irving Hoffman, Gerald Tegha, Deborah Kamwendo, Mitch Matoga, Sergei L Kosakovsky Pond, Myron S Cohen, Ronald Swanstrom",
+      "publication": "Journal of virology",
+      "date": "2020",
+      "link": "https://jvi.asm.org/content/94/12/e00151-20.abstract"
+  },
+  "Analysis of unusual and signature APOBEC-mutations": {
+      "title": "Analysis of unusual and signature APOBEC-mutations in HIV-1 pol next-generation sequences",
+      "authors": "Philip L Tzou, Sergei L Kosakovsky Pond, Santiago Avila-Rios, Susan P Holmes, Rami Kantor, Robert W Shafer",
+      "publication": "PloS one",
+      "date": "2020",
+      "link": "https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0225352"
+  },
+  "Phyloanatomic characterization of the distinct T cell": {
+      "title": "Phyloanatomic characterization of the distinct T cell and monocyte contributions to the peripheral blood HIV population within the host",
+      "authors": "Brittany RifeMagalis, Samantha L Strickland, Stephen D Shank, Patrick Autissier, Alexandra Schuetz, Pasiri Sithinamsuwan, Sukalaya Lerdlum, James LK Fletcher, Mark de Souza, Jintanat Ananworanich, Victor Valcour, Search007 Study Group, Kenneth C Williams, Sergei L Kosakovsky Pond, Silvia RattoKim, Marco Salemi",
+      "publication": "Virus evolution",
+      "date": "2020",
+      "link": "https://academic.oup.com/ve/article-abstract/6/1/veaa005/5825710"
+  },
+  "Reservoir Established near Therapy Initiation": {
+    "title": "The Replication-Competent HIV-1 Latent Reservoir is Primarily Established Near the Time of Therapy Initiation",
+    "authors": "Melissa-Rose Abrahams, Sarah B. Joseph, Nigel Garrett, Lynn Tyers, Matthew Moeser, Nancie Archin, Olivia D. Council, David Matten, Shuntai Zhou, Deelan Doolabh, Colin Anthony, Nilu Goonetilleke, Salim Abdool Karim, David M. Margolis, Sergei Kosakovsky Pond, Carolyn Williamson, Ronald Swanstrom",
+    "publication": "Science Trans Med",
+    "date": "2019",
+    "link": "https://stm.sciencemag.org/content/11/513/eaaw5589"
+  },
+  "Northern California Clinical Cohort": {
+    "title": "National and International Dimensions of HIV-1 Sequence Clusters in a Northern California Clinical Cohort",
+    "authors": "Soo-Yon Rhee, Brittany R Magalis, Leo Hurley, Michael J Silverberg, Julia L Marcus, Sally Slome, Sergei L Kosakovsky Pond and Robert W Shafer",
+    "publication": "Open Forum Infectious Diseases",
+    "date": "2019",
+    "link": "https://academic.oup.com/ofid/advance-article/doi/10.1093/ofid/ofz135/5380637"
+  },
+  "Stable Intermediates and RNA Structrual Heterogeneity": {
+    "title": "Compensatory Evolution within Viral RNA Structure: The Potential Role of Stable Intermediates in RNA Structural Heterogeneity",
+    "authors": "Brittany Rife Magalis, Blanton S Tolbert, Marco Salemi, and Sergei L Kosakovsky Pond",
+    "publication": "FASEB Journal",
+    "date": "2019",
+    "link": "https://www.fasebj.org/doi/abs/10.1096/fasebj.2019.33.1_supplement.lb191"
+  },
+  "Hep B in East Asia": {
+    "title": "Origin and dissemination of hepatitis B virus genotype C in East Asia revealed by phylodynamic analysis and historical correlates",
+    "authors": "Serena Y. C. Lin, Brittany Rife Magalis, Marco Salemi, Hsin‐Fu Liu",
+    "publication": "Journal of Viral Hepatitis",
+    "date": "2018",
+    "link": "https://onlinelibrary.wiley.com/doi/full/10.1111/jvh.13006"
+  },
+  "HIV clusters in NYC": {
+    "title": "Growth of HIV-1 Molecular Transmission Clusters in New York City",
+    "authors": "Joel O Wertheim, Ben Murrell, Sanjay R Mehta, Lisa A Forgione, Pond SL Kosakovsky, Davey M Smith, Lucia V Torian",
+    "publication": "The Journal of infectious diseases",
+    "date": "2018",
+    "link": "https://academic.oup.com/jid/advance-article-abstract/doi/10.1093/infdis/jiy431/5053881?redirectedFrom=PDF"
+  },
+  "Motifs that Direct Fate": {
+    "title": "Identification of conserved, primary sequence motifs that direct retrovirus RNA fate",
+    "authors": "Gatikrushna Singh, Brittany D Rife, Bradley Seufzer, Marco Salemi, Aaron Rendahl, Kathleen Boris-Lawrie",
+    "publication": "Nucleic Acids Research",
+    "date": "2018",
+    "link": "https://academic.oup.com/nar/article/46/14/7366/5006033"
+  },
+  "Global Env within and among": {
+    "title": "Evaluation of global HIV/SIV envelope gp120 RNA structure and evolution within and among infected hosts",
+    "authors": "Brittany Rife Magalis, Sergei L Kosakovsky Pond, Michael F Summers, Marco Salemi",
+    "publication": "Virus Evolution",
+    "date": "2018",
+    "link": "https://academic.oup.com/ve/article/4/1/vey018/5042828"
+  },
+  "Tissue Reservoir": {
+    "title": "Persistent HIV-1 replication maintains the tissue reservoir during therapy",
+    "authors": "Ramon Lorenzo-Redondo, Helen R. Fryer, Trevor Bedford, Eun-Young Kim, John Archer, Sergei L. Kosakovsky Pond, Yoon-Seok Chung, Sudhir Penugonda, Jeffrey G. Chipman, Courtney V. Fletcher, Timothy W. Schacker, Michael H. Malim, Andrew Rambaut, Ashley T. Haase, Angela R. McLean, Steven M. Wolinsky",
+    "publication": "Nature",
+    "date": "2016",
+    "link": "https://www.nature.com/articles/nature16933"
+  },
+  "Compartmentalization": {
+    "title": "Comparative Study of Methods for Detecting Sequence Compartmentalization in Human Immunodeficiency Virus Type 1",
+    "authors": "Selene Zárate, Sergei L. Kosakovsky Pond, Paul Shapshak, and Simon D. W. Frost",
+    "publication": "Journal of Virology",
+    "date": "2007",
+    "link": "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC1900087/"
+  },
+}
